0306d9330c41abcc32d811ce336bfe0b8b02fc3c
Sourav <heysouravroy@gmail.com>
2025-04-29 17:12:01 +0530

    first commit

---
+++ ArraysAndArrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ArraysAndArrayList{
+    public static void main(String[] args){
+        
+        // Syntax
+        // datatyoe[] arrayName = new datatype[size];
+        int[] roll1 = new int[5]; // array of size 5
+        // OR
+        int[] roll2 = {1, 2, 3, 4, 5}; // array of size 5 with values
+        
+    }
+}
